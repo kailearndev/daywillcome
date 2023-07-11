e66c08705f944b83b54592ee92d6b7113f4e049c
luanvu-dev <>
2023-07-11 18:38:43 +0700

    create

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(9922);
 }
 bootstrap();
