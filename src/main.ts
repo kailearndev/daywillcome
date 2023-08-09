21c7afd76d12c6d2914bb2ba238226353ed6a99c
luanvu-dev <>
2023-08-09 16:34:17 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { createServer as createHttpServer } from 'http';
-import { createServer as createHttpsServer } from 'https';
-import * as express from 'express';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('./cert/private-key.pem'),
-  //   cert: fs.readFileSync('./cert/cer.pem'),
-  // };
-
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.init();
-  createHttpServer(server).listen(3000);
-  // createHttpsServer(server).listen(443);
+  await app.listen(3000);
 }
 bootstrap();
