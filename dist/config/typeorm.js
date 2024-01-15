96e0985fa1a5ed0144e29f7d817622cc74a1633a
kai <luanvuforwork@gmail.com>
2024-01-15 16:54:17 +0700

    revert code

--- dist/config/typeorm.js
+++ dist/config/typeorm.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeormConfig = void 0;
 exports.typeormConfig = {
     type: 'mysql',
-    host: '103.130.214.81',
+    host: '103.82.38.16',
     port: 3306,
-    username: 'kaiadmin',
+    username: 'admin',
     password: 'Nvl@2022',
-    database: 'daywillcomedb',
+    database: 'kai_db',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
 };
